65e887e65f2841844e94469dbb6eb066ffde30fc
Hasan Aslan <itshasanaslan@gmail.com>
2026-01-12 21:41:16 +0300

    12.01.2026 Monday 1.2

--- src/vending_machine/VendingMachine.java
+++ src/vending_machine/VendingMachine.java
@@ -1,5 +1,7 @@
 package vending_machine;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class VendingMachine {
@@ -25,7 +27,7 @@ public class VendingMachine {
             System.out.print("Enter an item number: ");
             System.out.println(user);
 
-            int choice = s.nextInt();
+            int choice = s.nextInt() - 1;
 
             if (choice < 0 || choice > this.items.length) {
                 System.out.println("Enter a valid item! Terminating!");
@@ -60,7 +62,61 @@ public class VendingMachine {
         return item.getStock() > 0;
     }
 
-    public static void run() {
+    public static Item[] loadItems() throws FileNotFoundException {
+
+        try {
+            // try read file and load items
+            Item[] items;
+
+            File inputFile = new File("vending_items.txt");
+            Scanner reader = new Scanner(inputFile);
+
+            int linesInTheFile = 0;
+
+            while (reader.hasNext()) {
+                linesInTheFile++;
+                reader.nextLine();
+            }
+
+            reader.close();
+
+            reader = new Scanner(inputFile);
+
+            int index = 0;
+            items = new Item[linesInTheFile];
+
+            while (reader.hasNext()) {
+                String[] line = reader.nextLine().split(",");
+                items[index] = new Item(line[0], Integer.parseInt(line[1]), Double.parseDouble(line[2]));
+                index++;
+            }
+
+            reader.close();
+            return items;
+
+        }
+
+        // else
+
+        catch (Exception e) {
+            System.out.println("File doesn't exist. Returning default items");
+            Item item1 = new Item("M&M", 10, 7.54);
+            Item item2 = new Item("Ice-cream", 5, 10.23);
+            Item item3 = new Item("Water", 5, 2.32);
+            Item item4 = new Item("Ice tea", 10, 2.54);
+            Item item5 = new Item("Chocolate", 1, 6.24);
+            Item item6 = new Item("Brownies", 3, 6.24);
+            Item item7 = new Item("Chips", 5, 6.24);
+            Item item8 = new Item("Coke", 2, 6.24);
+            Item item9 = new Item("Apple juice", 1, 6.24);
+            Item item10 = new Item("Orange juice", 1, 6.24);
+            Item[] items = { item1, item2, item3, item4, item5, item6, item7, item8, item9, item10 };
+            return items;
+
+        }
+    }
+
+    public static void run() throws FileNotFoundException{
 
         Item[] items = VendingMachine.loadItems();
         User user = new User("Jack", 55);
--- src/week_11/MyApp.java
+++ src/week_11/MyApp.java
@@ -2,36 +2,33 @@ package week_11;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
-
 import vending_machine.*;
 
 public class MyApp {
-    public static void main(String[] args) throws FileNotFoundException{
-
+    public static void main(String[] args) throws FileNotFoundException {
 
         // create file object
         File inputFile = new File("data.txt");
         // enable access for scanner to access the file
         Scanner scanner = new Scanner(inputFile);
-    
-
 
         int numOfLines = 0;
-        while (scanner.hasNext()){
-                numOfLines++;
-                // consume
-                scanner.nextLine();
+        while (scanner.hasNext()) {
+            numOfLines++;
+            // consume
+            scanner.nextLine();
         }
 
         System.out.println(numOfLines);
         User[] users = new User[numOfLines];
 
         scanner.close();
-        
+
         scanner = new Scanner(inputFile);
         int index = 0;
-        while (scanner.hasNext() ) {
+        while (scanner.hasNext()) {
             // scope is local
             String[] data = scanner.nextLine().split(",");
             User user = new User(data[0], Double.parseDouble(data[1]));
@@ -40,53 +37,46 @@ public class MyApp {
             System.out.println(user);
         }
 
-        
         System.out.println(users[0]);
         scanner.close();
 
-
-       VendingMachine.run();
+        VendingMachine.run();
 
     }
 
-    public static void sessionOne(){
-             // arrays
-
-       
+    public static void sessionOne() {
+        // arrays
 
         /*
-             double x = 4.31234324234;
-        String xAsString = String.format("%.4f" , x);
-        System.out.println(xAsString);
-        */
-
+         * double x = 4.31234324234;
+         * String xAsString = String.format("%.4f" , x);
+         * System.out.println(xAsString);
+         */
 
         Scanner s = new Scanner(System.in);
-          double value;
+        double value;
         try {
             // do something
             // if something goes wrong here
             // jump to the catch part
             value = s.nextDouble();
             // more
-            //lines
+            // lines
         }
 
-        catch (InputMismatchException e) {
+        catch (InputMismatchException es) {
             System.out.println("That's not a double");
         }
 
         catch (Exception e) {
-               System.out.println("Something else happened");
-                value = 1;
-            }
-
-        finally {
-
+            System.out.println("Something else happened");
+            value = 1;
         }
-            
+        finally {
+            System.out.println("This part will execute whatever happens. Not necessary");
+        }
 
         System.out.println("Program has finished");
-    
+
     }
 }
