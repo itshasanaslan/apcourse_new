3b15c0f69bb7b2a2dd97a8395da24d9aa0b0a66a
Hasan Aslan <itshasanaslan@gmail.com>
2026-03-14 10:37:41 +0300

    idk

---
+++ src/revision/Program.java
@@ -0,0 +1,26 @@
+package revision;
+
+import java.util.ArrayList;
+
+import helpers.ArrayHelpers;
+
+public class Program {
+    public  static int y; // Global variable
+    //scope
+    public static void main(String[] args){
+        // allocation, declaration
+        int x = 3;
+        System.out.println(x);
+        //scope
+           y = 15;
+    }
+
+    public  void hello(){
+        y = 14;
+        System.out.println(y);
+    }
+
+    list.sort
+
+    ArrayHelpers.merge()
+}
